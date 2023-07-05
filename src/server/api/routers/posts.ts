4739978a598a35f86c33d10984c19b44adf39fb5
IATec - Wictor Mendes Vieira <wictor.vieira@iatec.com>
2023-07-05 20:59:19 -0300

    Ratelimit in the endoint

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,11 +3,18 @@ import { type User } from "@clerk/nextjs/dist/types/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
 
 const filterUserFromClient = (user: User) => {
-
   return { id: user.id, username: user.username, profilePicture: user.profileImageUrl }
 }
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  prefix: "@upstash/ratelimit",
+});
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -44,6 +51,9 @@ export const postsRouter = createTRPCRouter({
   })).mutation(async ({ ctx, input }) => {
 
     const authorId = ctx.userId
+
+    const { success } = await ratelimit.limit(authorId)
+    if(!success) throw new TRPCError({code: "TOO_MANY_REQUESTS"})
 
     const post = await ctx.prisma.post.create({
       data: {
