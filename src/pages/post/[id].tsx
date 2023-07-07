01f38fe41fe22fb68574cb424131ce23825d6370
IATec - Wictor Mendes Vieira <wictor.vieira@iatec.com>
2023-07-06 23:29:58 -0300

    Full post page

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,14 +1,48 @@
 import Head from "next/head";
+import { api } from "~/utils/api";
+import { type GetStaticProps, type NextPage } from "next";
+import { PageLayout } from "~/components/layout";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
+import { PostView } from "~/components/postview";
 
-export default function SinglePage() {
+const SinglePostPage: NextPage<{ id: string }> = ({ id }) => {
+  const { data } = api.posts.getById.useQuery({
+    id,
+  });
+
+  if (!data) return <div>404</div>;
+
   return (
     <>
       <Head>
-        <title>Posts</title>
+        <title>{`${data.post.content} - @${data.author.username}`}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>Post View</div>
-      </main>
+      <PageLayout>
+        <PostView {...data} />
+      </PageLayout>
     </>
   );
-}
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const helpers = generateSSGHelper()
+
+  const id = context.params?.id;
+
+  if (typeof id !== "string") throw new Error("no slug");
+
+  await helpers.posts.getById.prefetch({ id });
+
+  return {
+    props: {
+      trpcState: helpers.dehydrate(),
+      id,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
+};
+
+export default SinglePostPage;
